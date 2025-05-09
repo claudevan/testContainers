44fd48ea2ea6ee32244b53a5b1bfbba50e93117b
Claudevan <claudevan.ti@gmail.com>
2025-05-08 21:21:02 -0300

    feature: incluindo log

--- testContainers/src/main/java/io/github/com/claudevan/testContainers/controllers/CepController.java
+++ testContainers/src/main/java/io/github/com/claudevan/testContainers/controllers/CepController.java
@@ -2,11 +2,15 @@ package io.github.com.claudevan.testcontainers.controllers;
 
 import io.github.com.claudevan.testcontainers.domain.CepDto;
 import io.github.com.claudevan.testcontainers.services.CepService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/cep")
 public class CepController {
@@ -19,6 +23,7 @@ public class CepController {
 
     @GetMapping("{cep}")
     public CepDto getAddress(@PathVariable String cep) {
+        log.info("Requesting address for CEP: {}", cep);
 
         return cepService.getAddress(cep);
     }
